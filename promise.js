abcc2d11dbf90ce987429659e488cbc0eeae10df
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-04 14:00:00 +0300

    first commit promise day 18

---
+++ promise.js
@@ -0,0 +1,6 @@
+// A promise is a way to handle asynchronous operation in js. 
+/** it allows handlers with an asynchronous action's eventual success value or failure reason 
+ * The asynchronous method instead of immediately returning the final value it returns a promise to supply the value at some point in the future
+ */
+
+// Promise constructor
